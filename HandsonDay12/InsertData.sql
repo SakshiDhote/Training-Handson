6ae544d657a6c6c067f697c20674d7efc44c07b5
SakshiDhote <78300161+SakshiDhote@users.noreply.github.com>
2022-08-01 09:46:01 +0530

    Update InsertData.sql

--- HandsonDay12/InsertData.sql
+++ HandsonDay12/InsertData.sql
@@ -1,1 +1,11 @@
 
+-- Insert one record without listing the column names in the insert statement. Check whether data is inserted
+
+--  Eg:
+
+--  employee_id first_name last_name department_id salary
+
+--  201 Michael Hartstein 20 13000
+
+
+insert into Employee values(201,'Michael','Hartstein',20,13000);
