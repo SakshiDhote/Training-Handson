bc292af3cd14b6e7da6fa670d1c96527e379f4e3
SakshiDhote <78300161+SakshiDhote@users.noreply.github.com>
2022-08-01 09:44:15 +0530

    Update CreateTable.sql

--- HandsonDay12/CreateTable.sql
+++ HandsonDay12/CreateTable.sql
@@ -1,1 +1,19 @@
 
+--  Test the table creation by viewing the structure using describe command
+
+--  Name Null Type
+
+--  ------------------------------ -------- ------------------------------
+
+--  EMPLOYEE_ID int(6)
+
+--  FIRST_NAME VARCHAR(20)
+
+--  LAST_NAME VARCHAR(25)
+
+--  DEPARTMENT_ID int(4)
+
+--  SALARY double
+
+
+create table Employee(EMPLOYEE_ID int(6), FIRST_NAME VARCHAR(20), LAST_NAME VARCHAR(25), DEPARTMENT_ID int(4),SALARY double);
