943b53a219a861baf7891bfb0bbde17d01e6d21f
SakshiDhote <78300161+SakshiDhote@users.noreply.github.com>
2022-08-01 09:48:38 +0530

    Update InsertMultipleData.sql

--- HandsonDay12/InsertMultipleData.sql
+++ HandsonDay12/InsertMultipleData.sql
@@ -1,1 +1,12 @@
 
+-- Insert one record without listing the column names in the insert statement where salary value remain undetermined. Check whether data is inserted
+
+--  Eg:
+
+--  employee_id first_name last_name department_id salary
+
+--  201 Michael Hartstein 20 13000
+
+--  202 Pat Fay 20 (null)
+
+insert into Employee values(202,'Pat','Fay',20,NULL);
