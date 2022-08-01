44c955709fc00ccd4bd39dcf0ee4f66b3ce60247
SakshiDhote <78300161+SakshiDhote@users.noreply.github.com>
2022-08-01 09:51:30 +0530

    Update InsertData3.sql

--- HandsonDay12/InsertData3.sql
+++ HandsonDay12/InsertData3.sql
@@ -1,1 +1,22 @@
+-- Use the above Script to insert the below given records
+
+--  employee_id first_name last_name department_id salary
+
+--  205 Shelley Higgins 110 12000
+
+--  100 Steven King 90 24000
+
+--  101 Neena Kochhar 90 17000
+
+--  102 Lex De Haan 90 17000
+
+--  111 Ismael Sciarra 100 7700
+
+--  112 Jose Manuel Urman 100 7800
+
+--  204 Hermann Baer 70 10000
+
+
+
+insert into Employee values(205,'Shelley','Higgins',110,1200),(100,'Steven','King',90,24000),(101 ,'Neena','Kochhar',90,17000),(102,'Lex','De',90,17000),(111,'Ismael','Sciarra',100,7700),(112,'Jose','Manuel Urman',100,7800),(204,'Hermann','Baer',70,10000);
 
