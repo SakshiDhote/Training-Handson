3422d64b8dbef01a188d5fb55ac57e604298940b
SakshiDhote <78300161+SakshiDhote@users.noreply.github.com>
2022-08-07 11:08:50 +0530

    Update SelectData.java

--- HandsonDay16/SelectData.java
+++ HandsonDay16/SelectData.java
@@ -1,3 +1,13 @@
+/*Display Student details
+
+Write the code to display details of all the students, if no roll no. is passed, while executing the main program.
+
+If while executing the main program, the roll no. is passed, then it should display the record of that particular student.
+
+
+
+*/
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
